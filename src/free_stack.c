f0a1b56f9ab45f0473916431f3c098714f2a1384
hali-mah <hichalimahdi@gmail.com>
2024-11-25 19:22:16 +0100

    Trying a simple sorting script

--- src/free_stack.c
+++ src/free_stack.c
@@ -1,32 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   free_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/25 18:18:00 by hali-mah          #+#    #+#             */
-/*   Updated: 2024/11/25 19:15:17 by hali-mah         ###   ########.fr       */
+/*   Created: 2024/11/25 19:20:17 by hali-mah          #+#    #+#             */
+/*   Updated: 2024/11/25 19:21:20 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "../include/push_swap.h"
 
-# include "../ft_printf/ft_printf.h"
-# include <stdio.h>
-# include <stdlib.h>
-
-typedef struct s_node
+void	free_stack(t_stack *stack)
 {
-	int				value;
-	struct s_node	*next;
-}	t_node;
-
-typedef struct s_stack
-{
-	t_node	*top;
-	int		size;
-}	t_stack;
-
-#endif
+	while (stack->size > 0)
+		pop(stack);
+	free(stack);
+}
---
+++ src/stack.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 18:57:58 by hali-mah          #+#    #+#             */
+/*   Updated: 2024/11/25 19:20:05 by hali-mah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+t_stack	*init_stack(void)
+{
+	t_stack	*stack;
+
+	stack = (t_stack *)malloc(sizeof(t_stack));
+	stack->top = NULL;
+	stack->size = 0;
+	return (stack);
+}
+
+void	push(t_stack *stack, int value)
+{
+	t_node	*new_node;
+
+	new_node = (t_node *)malloc(sizeof(t_node));
+	new_node->value = value;
+	new_node->next = stack->top;
+	stack->top = new_node;
+	stack->size++;
+}
+
+int	pop(t_stack *stack)
+{
+	t_node	*temp;
+	int		value;
+
+	if (stack->size == 0)
+		return (-1);
+	temp = stack->top;
+	value = temp->value;
+	stack->top = stack->top->next;
+	free(temp);
+	stack->size--;
+	return (value);
+}
+
+void	swap(t_stack *stack)
+{
+	t_node	*first;
+	t_node	*second;
+
+	if (stack->size < 2)
+		return ;
+	first = stack->top;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	stack->top = second;
+}
+
+void	print_stack(t_stack *stack)
+{
+	t_stack	*current;
+
+	current = stack->top;
+	ft_printf("Stack (top to bottom):\n");
+	while (current)
+	{
+		ft_printf("%d\n", current->value);
+		current = current->next;
+	}
+	ft_printf("Size: %d\n", stack->size);
+}
